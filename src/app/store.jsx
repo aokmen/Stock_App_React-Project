497f3d8613b200eb014c1b62ff5db7347374c6fa
aokmen <abdullah.okmen.11@gmail.com>
2023-09-11 21:45:43 +0200

    store _file_updated

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import stockReducer from "../features/stockSlice";
-import { persistStore, persistReducer } from "redux-persist";
+// import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const persistConfig = {
   key: "root",// storage veriler key-value şeklinde saklanır. Buraki key storagedaki keyi temsil ediyor.
@@ -14,10 +24,20 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
-    stock:stockReducer,
-  }, 
+    stock: stockReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
 export let persistor = persistStore(store);
 export default store;
+
+//! prop drilling
+//! Global State Management (Redux, Mobx, Zustand, Context API)
+//? Context API native bir yapı. Yani reactin kendisine ait.
